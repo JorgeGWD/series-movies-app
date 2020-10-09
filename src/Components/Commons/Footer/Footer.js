f60ae3928635b400b28baf41e630925f3c87a2ad
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-10-09 12:24:54 -0500

    Footer style

--- src/Components/Commons/Footer/Footer.js
+++ src/Components/Commons/Footer/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import './Footer.css'
 import LogoFooter from '../../../assets/logo_footer.png'
+import GooglePLay from '../../../assets/google-play.jpg'
 import NavMenu from '../NavMenu/NavMenu'
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
-                <NavMenu />
-                <img src={LogoFooter} alt="Logo" />
-                <p>&copy; {new Date().getFullYear()} Todos los derechos reservados</p>
+                <div className="foter-left">
+                    <NavMenu />
+                    <img src={LogoFooter} alt="Logo" />
+                    <p>&copy; {new Date().getFullYear()} Todos los derechos reservados</p>
+                </div>
+                <img src={GooglePLay} alt="google-play" />
             </div>
         </footer>
     )
