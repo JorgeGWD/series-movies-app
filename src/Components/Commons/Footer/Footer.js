37b9e6a27bcbd6770bf8bee9577b0d1ca1869ea0
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-10-09 11:34:56 -0500

    Create NavMenu component

--- src/Components/Commons/Footer/Footer.js
+++ src/Components/Commons/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Footer.css'
+import LogoFooter from '../../../assets/logo_footer.png'
+import NavMenu from '../NavMenu/NavMenu'
 
 const Footer = () => {
     return (
         <footer>
             <div className="footer-content">
-                
+                <NavMenu />
+                <img src={LogoFooter} alt="Logo" />
             </div>
         </footer>
     )
---
+++ src/Components/Commons/NavMenu/NavMenu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NavMenu.css'
+
+const NavMenu = () => {
+    return (
+        <div className="nav-menu">
+            <ul>
+                <li><a href="jsx-a11y/anchor-is-valid">Home</a></li>
+                <li><a href="jsx-a11y/anchor-is-valid">Terms and Conditions</a></li>
+                <li><a href="jsx-a11y/anchor-is-valid">Privacy Policy</a></li>
+                <li><a href="jsx-a11y/anchor-is-valid">Collection Statement</a></li>
+                <li><a href="jsx-a11y/anchor-is-valid">Help</a></li>
+                <li><a href="jsx-a11y/anchor-is-valid">Manage Account</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default NavMenu
