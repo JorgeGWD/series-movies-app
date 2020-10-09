dabc38ae1ff6d022f83e98966b5d4ff5aaad9f65
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-10-09 11:05:11 -0500

    Create Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Footer from './Components/Commons/Footer/Footer';
 import Header from './Components/Commons/Header/Header';
 import TitleBar from './Components/Commons/TitleBar/TitleBar';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <TitleBar />
+      <Footer />
     </div>
   );
 }
