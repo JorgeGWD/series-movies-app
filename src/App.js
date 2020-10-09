dda12a2f290a0ce029bcacf004a34e16ec9ac4fb
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-10-09 11:12:55 -0500

    Create Home component, Header z-index

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './Components/Commons/Footer/Footer';
 import Header from './Components/Commons/Header/Header';
 import TitleBar from './Components/Commons/TitleBar/TitleBar';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <TitleBar />
+      <Home />
       <Footer />
     </div>
   );
