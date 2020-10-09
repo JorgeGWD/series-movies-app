ab840a8d80e478d1bccfc1dd71764232ce4fee3e
JorgeGWD <jorge.rodriguez.r@gmail.com>
2020-10-09 10:58:15 -0500

    Create Title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Commons/Header/Header';
+import TitleBar from './Components/Commons/TitleBar/TitleBar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TitleBar />
     </div>
   );
 }
---
+++ src/Components/Commons/TitleBar/TitleBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TitleBar.css'
+
+const TitleBar = () => {
+    return (
+        <div className="title-bar">
+            <div className="title-bar-content">
+                <h1>Popular Titles</h1>
+            </div>
+        </div>
+    )
+}
+
+export default TitleBar
